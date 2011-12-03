5a35d69eda8b4d22dee30b5bdf77a955f9989d1b
vtk666 <devnull@localhost>
2011-12-04 00:01:28 +0600

    optimization?

--- src/common/map_tiled.js
+++ src/common/map_tiled.js
@@ -28,20 +28,22 @@ MapTiled.prototype.add = function(item)
         console.trace();
         throw {message: 'item.id must be set', item: item};
     }
-    var fromX = Math.floor((item.x-item.hw) / this.tileSize);
-    if (fromX < 0) fromX = 0; else if (fromX > this.maxX) fromX = this.maxX;
-    var toX   = Math.ceil ((item.x+item.hw) / this.tileSize);
-    if (toX < 0) toX = 0; else if (toX > this.maxX) toX = this.maxX;
-    var fromY = Math.floor((item.y-item.hh) / this.tileSize);
-    if (fromY < 0) fromY = 0; else if (fromY > this.maxX) fromY = this.maxY;
-    var toY   = Math.ceil ((item.y+item.hh) / this.tileSize);
-    if (toY < 0) toY = 0; else if (toY > this.maxX) toY = this.maxY;
-    for (var x = fromX ; x <= toX ; x++) {
-        for (var y = fromY ; y <= toY ; y++) {
-            this.items[x][y][item.id] = item;
-        }
+    if (this.all[item.id] === undefined) {
+	    var fromX = Math.floor((item.x-item.hw) / this.tileSize);
+	    if (fromX < 0) fromX = 0; else if (fromX > this.maxX) fromX = this.maxX;
+	    var toX   = Math.ceil ((item.x+item.hw) / this.tileSize);
+	    if (toX < 0) toX = 0; else if (toX > this.maxX) toX = this.maxX;
+	    var fromY = Math.floor((item.y-item.hh) / this.tileSize);
+	    if (fromY < 0) fromY = 0; else if (fromY > this.maxX) fromY = this.maxY;
+	    var toY   = Math.ceil ((item.y+item.hh) / this.tileSize);
+	    if (toY < 0) toY = 0; else if (toY > this.maxX) toY = this.maxY;
+	    for (var x = fromX ; x <= toX ; x++) {
+	        for (var y = fromY ; y <= toY ; y++) {
+	            this.items[x][y].push(item);
+	        }
+	    }
+	    this.all[item.id] = item;
     }
-    this.all[item.id] = item;
 };
 
 MapTiled.prototype.get = function(id)
@@ -63,7 +65,11 @@ MapTiled.prototype.remove = function(item)
         if (toY < 0) toY = 0; else if (toY > this.maxX) toY = this.maxY;
         for (var x = fromX ; x <= toX ; x++) {
             for (var y = fromY ; y <= toY ; y++) {
-                delete this.items[x][y][item.id];
+            	for (var i = this.items[x][y].length - 1 ; i >= 0 ; i--) {
+            		if (this.items[x][y][i].id == item.id) {
+            			this.items[x][y].splice(i, 1);
+            		}
+            	}
             }
         }
         return true;
@@ -117,7 +123,7 @@ MapTiled.prototype.intersects = function(item, ix, iy, hw, hh)
     if (toY < 0) toY = 0; else if (toY > this.maxX) toY = this.maxY;
     for (var x = fromX ; x <= toX ; x++) {
         for (var y = fromY ; y <= toY ; y++) {
-            for (var i in this.items[x][y]) {
+            for (var i = this.items[x][y].length - 1 ; i >= 0 ; i--) {
                 var each = this.items[x][y][i];
                 if (each.id == item.id || res[each.id]) continue;
                 if (Math.abs(each.x - ix) < (each.boundX + hw) &&
