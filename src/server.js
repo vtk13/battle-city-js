33f8ef92ea7cf6d4c02f9ce4cb915ec765d34c6e
vtk666 <devnull@localhost>
2011-12-08 23:54:27 +0600

    command line "serve-static" argiment to serve static content or not

--- src/server.js
+++ src/server.js
@@ -56,9 +56,47 @@ process.on('uncaughtException', function(ex) {
  * Server is responsible for accepting user connections.
  */
 var server = require('http').createServer(function(request, response) {
-    // static served with haproxy + nginx
-    response.writeHead(301, {'Location':'http://'+request.headers.host.replace(/:\d+$/, '')});
-    response.end();
+    if (process.argv.indexOf('serve-static') == -1) {
+        // static served with haproxy + nginx
+        response.writeHead(301, {'Location':'http://'+request.headers.host.replace(/:\d+$/, '')});
+        response.end();
+    } else {
+        var uri = url.parse(request.url).pathname;
+        if (uri == '/') {
+            uri = '/index.html';
+        }
+        var filename = path.join(process.cwd(), uri);
+        path.exists(filename, function(exists) {
+            if(!exists) {
+                response.writeHead(404, {"Content-Type": "text/plain"});
+                response.write("404 Not Found\n");
+                response.end();
+                return;
+            }
+
+            fs.readFile(filename, "binary", function(err, file) {
+                if(err) {
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write(err + "\n");
+                    response.end();
+                    return;
+                }
+
+                var exts = {
+                        '.html': 'text/html',
+                        '.js': 'application/x-javascript',
+                        '.png': 'image/png',
+                        '.css': 'text/css'
+                };
+                response.writeHead(200, {
+                    'Content-Type': exts[path.extname(filename)]
+                });
+                response.write(file, "binary");
+                response.end();
+            });
+        });
+    }
+
 });
 server.listen(8124);
 
