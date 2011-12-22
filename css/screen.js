a0129d488ea3ea31c50b73a872f0dff4c4e78b7d
vtk666 <devnull@localhost>
2011-12-22 18:20:13 +0600

    add mainmenu div

--- css/screen.js
+++ css/screen.js
@@ -1,6 +1,7 @@
 
 $(window).resize(function(){
-    $('.chat-log').height($(this).height() - 70);
-    $('#bot-editor').height($(this).height() - $('#game').height());
+    $('#body').height($(this).height() - $('#mainmenu').height());
+    $('.chat-log').height($('#body').height() - 70);
+    $('#bot-editor').height($('#body').height() - $('#game').height());
     $('#bot-editor #editor').height($('#bot-editor').height()-2).width($('#bot-editor').width()-2);
 });
