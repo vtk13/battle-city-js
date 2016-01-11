f0f0c560ea671c6b6c7048402fdc42b7c68d4a28
Rogov Viktor <vtk@vda-team.com>
2016-01-11 22:10:57 +0300

    pack node_modules in the separate bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+var path = require('path');
+var SplitByPathPlugin = require('webpack-split-by-path');
+
 module.exports = {
     entry: {
         client: "src/client/init.js",
         test: "src/test.js"
     },
     output: {
-        filename: "[name].js"
+        filename: "[name].js",
+        chunkFilename: "[name].js"
     },
+    plugins: [
+        new SplitByPathPlugin([
+            {
+                name: 'vendor',
+                path: path.join(__dirname, 'node_modules')
+            }
+        ])
+    ],
     resolve: {
         root: [
             __dirname
